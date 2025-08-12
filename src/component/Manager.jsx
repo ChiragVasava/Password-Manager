e11c147dda10943077606f1481541cb478127080
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-12 23:44:10 +0530

    Update Manager.jsx

--- src/component/Manager.jsx
+++ src/component/Manager.jsx
@@ -8,12 +8,12 @@ const Manager = () => {
 
     useEffect(() => {
         let passwords = localStorage.getItem("passwords");
-        
-        if(passwords) {
+
+        if (passwords) {
             setPasswordArray(JSON.parse(passwords))
         }
     }, [])
-    
+
 
 
     const showPassword = () => {
@@ -29,7 +29,7 @@ const Manager = () => {
 
     const savePassword = () => {
         console.log(form);
-        passwordArray([...passwordArray, form])
+        setPasswordArray([...passwordArray, form])
         localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
         console.log([...passwordArray, form]);
     }
@@ -84,6 +84,32 @@ const Manager = () => {
                             </lord-icon>
                             Add Password</button>
                     </div>
+                </div>
+
+
+                <div className="passwords">
+                    <h2 className="font-bold text-2xl py-4">Your Passwords</h2>
+                    {passwordArray.length === 0 && <div> No Passwords To Show </div>}
+                    {passwordArray.length != 0 &&
+                        <table className="table-auto w-full rounded-md overflow-hidden">
+                            <thead className="bg-green-700 text-white">
+                                <tr>
+                                    <th className="py-2">Site</th>
+                                    <th className="py-2">Username</th>
+                                    <th className="py-2">Password</th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-green-100">
+                                {passwordArray.map((item, index)=> {
+                                    return <tr key={index}>
+                                    <td className="py-2 border border-white text-center w-32"><a href={item.site} target='_blank'>{item.site}</a></td>
+                                    <td className="py-2 border border-white text-center w-32">{item.username}</td>
+                                    <td className="py-2 border border-white text-center w-32">{item.password}</td>
+                                </tr>
+                                })}
+                            </tbody>
+                        </table>}
+
                 </div>
             </div>
         </>
