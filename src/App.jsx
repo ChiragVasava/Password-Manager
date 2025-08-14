bb8e47d95764544033e22f0179cd7c70d14441fc
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-14 18:53:49 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Navbar from './component/Navbar'
 import Manager from './component/Manager'
+import Footer from './component/Footer'
 import './App.css'
 
 function App() {
 
-
   return (
     <>
-    <Navbar/>
-<Manager/>
-
+      {/* BG.IBELICK Background */}
+      <div className="inset-0 -z-10 h-full w-full bg-green [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#22c55e_100%)]">
+        <Navbar />
+        <Manager />
+        <Footer />
+      </div>
     </>
   )
 }
